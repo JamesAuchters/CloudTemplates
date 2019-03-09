5ae63ac40b56f990c3433c3b71d723e0b85b6c2a
James <5344599+JamesAuchters@users.noreply.github.com>
2019-03-09 22:14:16 +1100

    Completed Initial Server Build, resolved formatting issues, changed deployment orger

--- CloudLab/Deploy.ps1
+++ CloudLab/Deploy.ps1
@@ -1,11 +1,13 @@
-$deploymentName = "CloudLab"
+$deploymentName = "CloudLabDeployment-" + "$(Get-Date -Format hhmm-ddMMyyyy)"
 
 #Sign in
 Write-Host "Logging in..." -ForegroundColor Green
 Login-AzAccount;
 
-New-AzDeployment  -name "$($deploymentName)-RGDeployment" -TemplateFile ResourceGroup.json
+New-AzDeployment  -name "$($deploymentName)-RGDeployment2" -TemplateFile ResourceGroup.json -Location australiaeast
 
 New-AzResourceGroupDeployment -ResourceGroupName Network-RG01 -Name "$($deploymentName)-Network" -TemplateFile .\NetworkResources.json
 
-New-AzResourceGroupDeployment -ResourceGroupName ANALYTICS-RG011 -Name "$($deploymentName)-Analytics" -TemplateFile .\Analytics.json
+New-AzResourceGroupDeployment -ResourceGroupName ANALYTICS-RG01 -Name "$($deploymentName)-Analytics" -TemplateFile .\Analytics.json
+
+New-AzResourceGroupDeployment -ResourceGroupName SERVER-RG01 -Name "$($deploymentName)-Servers" -TemplateFile .\servers.json
