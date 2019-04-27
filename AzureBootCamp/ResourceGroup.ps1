258a2c8a83c516ccff60a837191b7f454b292b41
James Auchterlonie <james.auchterlonie@xello.com.au>
2019-04-27 14:24:31 +1000

    hi

--- AzureBootCamp/ResourceGroup.ps1
+++
@@ -1,7 +0,0 @@
-az group create \
-    --name AzureBootCamp-RG01 \
-    --location australiaeast
-
-az group deployment create --resource-group AzureBootCamp-RG01 --template-file ./StorageAccounts.json
-
-az group
