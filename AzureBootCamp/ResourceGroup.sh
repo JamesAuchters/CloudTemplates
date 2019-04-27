de793c959d31a6cde4ced1b24f4c5815e6c7ff3e
James Auchterlonie <james.auchterlonie@xello.com.au>
2019-04-27 12:27:44 +1000

    god damn it

--- AzureBootCamp/ResourceGroup.sh
+++ AzureBootCamp/ResourceGroup.sh
@@ -2,4 +2,4 @@ az group create \
     --name AzureBootCamp-RG01 \
     --location australiaeast
 
-az group deployment create --resource-group AzureBootCamp-RG01 --template-file .git /StorageAccounts.json
+az group deployment create --resource-group AzureBootCamp-RG01 --template-file ./StorageAccounts.json
