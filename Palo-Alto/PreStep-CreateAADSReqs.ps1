04d91d0777717e099fdb55f4325af42b36a8c243
James Auchterlonie <5344599+JamesAuchters@users.noreply.github.com>
2019-07-03 23:54:42 +1000

    added aadds

---
+++ Palo-Alto/PreStep-CreateAADSReqs.ps1
@@ -0,0 +1,38 @@
+# Change the following values to match your deployment.
+$AaddsAdminUserUpn = "admin@contoso100.onmicrosoft.com"
+$AzureSubscriptionId = "YOUR_AZURE_SUBSCRIPTION_ID"
+$ManagedDomainName = "contoso100.com"
+$ResourceGroupName = "ACTIVEDIRECTORY-RG01"
+$AzureLocation = "australiaeast"
+$aadTenantId = "<Active Directory ID>"
+
+# Connect to your Azure Account.
+Connect-AzureAD -TenantId $aadTenantId
+
+# Login to your Azure subscription.
+Connect-AzAccount
+
+# Create the service principal for Azure AD Domain Services.
+New-AzureADServicePrincipal -AppId "2565bd9d-da50-47d4-8b85-4c97f669dc36"
+
+# Create the delegated administration group for AAD Domain Services.
+New-AzureADGroup -DisplayName "AAD DC Administrators" `
+  -Description "Delegated group to administer Azure AD Domain Services" `
+  -SecurityEnabled $true -MailEnabled $false `
+  -MailNickName "AADDCAdministrators"
+
+# First, retrieve the object ID of the newly created 'AAD DC Administrators' group.
+$GroupObjectId = Get-AzureADGroup `
+  -Filter "DisplayName eq 'AAD DC Administrators'" | `
+  Select-Object ObjectId
+
+# Now, retrieve the object ID of the user you'd like to add to the group.
+$UserObjectId = Get-AzureADUser `
+  -Filter "UserPrincipalName eq '$AaddsAdminUserUpn'" | `
+  Select-Object ObjectId
+
+# Add the user to the 'AAD DC Administrators' group.
+Add-AzureADGroupMember -ObjectId $GroupObjectId.ObjectId -RefObjectId $UserObjectId.ObjectId
+
+# Register the resource provider for Azure AD Domain Services with Resource Manager.
+Register-AzResourceProvider -ProviderNamespace Microsoft.AAD
