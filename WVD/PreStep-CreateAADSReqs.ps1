e891ca30272a718990bd2ae91219762973085f15
James Auchterlonie <james@macbook-pro.internal.cbussuper.com.au>
2020-03-11 12:35:50 +1100

    WVD setup

---
+++ WVD/PreStep-CreateAADSReqs.ps1
@@ -0,0 +1,39 @@
+# Change the following values to match your deployment.
+$AaddsAdminUserUpn = "<ADMIN NAME - Office 365>"
+$AzureSubscriptionId = "YOUR_AZURE_SUBSCRIPTION_ID"
+$ManagedDomainName = "<AADS Domain Name>"
+$ResourceGroupName = "ACTIVEDIRECTORY-RG01"
+$AzureLocation = "australiasoutheast"
+$aadTenantId = "<Active Directory ID>"
+
+# Connect to your Azure Account.
+Connect-AzureAD -TenantId $aadTenantId
+
+# Login to your Azure subscription.
+Connect-AzAccount -Tenant $aadTenantId
+Select-AzSubscription -SubscriptionId $AzureSubscriptionId
+
+# Create the service principal for Azure AD Domain Services.
+New-AzureADServicePrincipal -AppId "2565bd9d-da50-47d4-8b85-4c97f669dc36"
+
+# Create the delegated administration group for AAD Domain Services.
+New-AzureADGroup -DisplayName "AAD DC Administrators" `
+  -Description "Delegated group to administer Azure AD Domain Services" `
+  -SecurityEnabled $true -MailEnabled $false `
+  -MailNickName "AADDCAdministrators"
+
+# First, retrieve the object ID of the newly created 'AAD DC Administrators' group.
+$GroupObjectId = Get-AzureADGroup `
+  -Filter "DisplayName eq 'AAD DC Administrators'" | `
+  Select-Object ObjectId
+
+# Now, retrieve the object ID of the user you'd like to add to the group.
+$UserObjectId = Get-AzureADUser `
+  -Filter "UserPrincipalName eq '$AaddsAdminUserUpn'" | `
+  Select-Object ObjectId
+
+# Add the user to the 'AAD DC Administrators' group.
+Add-AzureADGroupMember -ObjectId $GroupObjectId.ObjectId -RefObjectId $UserObjectId.ObjectId
+
+# Register the resource provider for Azure AD Domain Services with Resource Manager.
+Register-AzResourceProvider -ProviderNamespace Microsoft.AAD
